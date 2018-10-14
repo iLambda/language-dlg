5a9d45859d385c8e1c5d4f6ed028e923388e4383
iLambda <me@ilambda.io>
2018-10-14 02:06:43 +0200

    Added atom syntax highlighting package

---
+++ highlighting/atom/spec/language-dlg-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageDlgView from '../lib/language-dlg-view';
+
+describe('LanguageDlgView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
